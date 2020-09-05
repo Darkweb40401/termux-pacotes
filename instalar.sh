50886d8512613571f42c27480493ceaf78fe28f6
Darkweb40401 <70810597+Darkweb40401@users.noreply.github.com>
2020-09-05 11:52:14 -0300

    instalar.sh

---
+++ instalar.sh
@@ -0,0 +1,214 @@
+#!/bin/bash
+
+clear
+
+echo "#####################"
+echo "CRIADOR -Darkweb40401"
+echo "#####################"
+echo "                     "
+echo "##########"
+echo "APT UPDATE"
+echo "##########"
+echo "          "
+apt update
+
+clear
+
+echo "#####################"
+echo "CRIADOR -Darkweb40401"
+echo "#####################"
+echo "                     "
+echo "###########"
+echo "PKG UPGRADE"
+echo "###########"
+echo "           "
+pkg upgrade -y
+
+clear
+
+echo "#####################"
+echo "CRIADOR -Darkweb40401"
+echo "#####################"
+echo "                     "
+echo "#####################"
+echo "PKG INSTALL ROOT-REPO"
+echo "#####################"
+echo "                     "
+pkg install root-repo
+
+clear
+
+echo "#####################"
+echo "CRIADOR -Darkweb40401"
+echo "#####################"
+echo "                     "
+echo "#########################"
+echo "PKG INSTALL UNSTABLE-REPO"
+echo "#########################"
+echo "                         "
+pkg install unstable-repo
+
+clear
+
+echo "#####################"
+echo "CRIADOR -Darkweb40401"
+echo "#####################"
+echo "                     "
+echo "####################"
+echo "PKG INSTALL X11-REPO"
+echo "####################"
+echo "                    "
+pkg install x11-repo
+
+clear
+
+echo "#####################"
+echo "CRIADOR -Darkweb40401"
+echo "#####################"
+echo "                     "
+echo "################"
+echo "PKG INSTALL NANO"
+echo "################"
+echo "                "
+pkg install nano -y
+
+clear
+
+echo "#####################"
+echo "CRIADOR -Darkweb40401"
+echo "#####################"
+echo "                     "
+echo "###############"
+echo "PKG INSTALL VIM"
+echo "###############"
+echo "              "
+pkg install vim -y
+
+clear
+
+echo "#####################"
+echo "CRIADOR -Darkweb40401"
+echo "#####################"
+echo "                     "
+echo "################"
+echo "PKG INSTALL CURL"
+echo "################"
+pkg install curl
+
+clear
+
+echo "#####################"
+echo "CRIADOR -Darkweb40401"
+echo "#####################"
+echo "                     "
+echo "################"
+echo "PKG INSTALL WGET"
+echo "################"
+pkg install wget -y
+
+clear
+
+echo "#####################"
+echo "CRIADOR -Darkweb40401"
+echo "#####################"
+echo "                     "
+echo "##################"
+echo "PKG INSTALL FIGLET"
+echo "##################"
+pkg install figlet
+
+clear
+
+echo "#####################"
+echo "CRIADOR -Darkweb40401"
+echo "#####################"
+echo "                     "
+echo "##################"
+echo "PKG INSTALL PYTHON"
+echo "##################"
+pkg install python -y
+
+clear
+
+echo "#####################"
+echo "CRIADOR -Darkweb40401"
+echo "#####################"
+echo "                     "
+echo "###################"
+echo "PKG INSTALL PYTHON2"
+echo "###################"
+pkg install python2
+
+clear
+
+echo "#####################"
+echo "CRIADOR -Darkweb40401"
+echo "#####################"
+echo "                     "
+echo "###############"
+echo "PKG INSTALL TOR"
+echo "###############"
+pkg install tor -y
+
+clear
+
+echo "#####################"
+echo "CRIADOR -Darkweb40401"
+echo "#####################"
+echo "                     "
+echo "#################"
+echo "PKG INSTALL CLANG"
+echo "#################"
+pkg install clang -y
+
+clear
+
+echo "#####################"
+echo "CRIADOR -Darkweb40401"
+echo "#####################"
+echo "                     "
+echo "################"
+echo "PKG INSTALL RUBY"
+echo "################"
+pkg install ruby -y
+
+clear
+
+echo "#####################"
+echo "CRIADOR -Darkweb40401"
+echo "#####################"
+echo "                     "
+echo "###############"
+echo "PKG INSTALL PHP"
+echo "###############"
+pkg install php -y
+
+clear
+
+echo "#####################"
+echo "CRIADOR -Darkweb40401"
+echo "#####################"
+echo "                     "
+echo "################"
+echo "PKG INSTALL PERL"
+echo "################"
+pkg install perl
+
+clear
+
+echo "#####################"
+echo "CRIADOR -Darkweb40401"
+echo "#####################"
+echo "                     "
+echo "###################"
+echo "PKG INSTALL OPENSSH"
+echo "###################"
+pkg install openssh -y
+echo "                     "
+echo "########################"
+echo "CRIADOR -Darkweb40401   "
+echo "                        "
+echo "TUDO INSTALADO          "
+echo "                        "
+echo "TELEGRAM - @Darkweb40401"
+echo "########################"
